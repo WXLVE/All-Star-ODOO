2ecdb819c0704f3e1d2dfdbbf8d0dc9eefbcfa80
Daniel Smit <daniel.smit.nl@gmail.com>
2023-08-23 16:23:47 +0200

    Poss FIX <> Cube

--- base_module/__manifest__.py
+++ base_module/__manifest__.py
@@ -1,15 +1,18 @@
 {
-    'name': 'Cube',  # The name that will appear in the App list
-    'version': '16.0',  # Version
-    'application': True,  # This line says the module is an App, and not a module
-    'depends': ["base"],  # dependencies
+    "name": "Cube",  # The name that will appear in the App list
+    "version": "16.0",  # Version
+    "application": True,  # This line says the module is an App, and not a module
+    'depends': [
+        "base"
+        
+    ],  # dependencies
 
     'data': [
         'security/ir.model.access.csv',
         'views/cube_property_views.xml',
         'views/cube_menus.xml',
     ],
+    "installable": True,
+    'license': 'LGPL-3',
 
-    'installable': True,
-    # 'license': 'LGPL-3',
 }
