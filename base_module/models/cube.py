57d8537a760eee65d9a5164491f506be5f974f1b
BierSaus <ian.spanje@student.graafschapcollege.nl>
2023-08-23 17:15:34 +0200

    remaining fields implemented

--- base_module/models/cube.py
+++ base_module/models/cube.py
@@ -5,4 +5,14 @@ class TestModel(models.Model):
     _description = "Model for cubes"
 
     name = fields.Char(required = True)
-    expected_price = fields.Float("Expected Price", required=True)
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Date Availability')
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling Price')
+    bedrooms = fields.Integer('Bedrooms')
+    living_area = fields.Integer('Living Area')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area')
+    garden_orientation = fields.Selection('Garden Orientation')
