316c36201c18bd4dd75e68e84460c387169f0923
Daniel Smit <daniel.smit.nl@gmail.com>
2023-08-24 14:14:23 +0200

    Small change

--- base_builder/__manifest__.py
+++ base_builder/__manifest__.py
@@ -11,24 +11,13 @@
     'data': [
         # Snippet Views
         'views/snippets/s_sliding_carousel.xml',
-        'views/snippets/velo_slider.xml',
 
-        # security
-        'security/ir.model.access.csv',
-
-        # views
-        'views/pear_offers.xml',
-        'views/pear_tags.xml',
-        'views/pear_types.xml',
-        'views/pear_views.xml',
-        "views/res_users_view.xml",
-        'views/base_module_menus.xml',
     ],
-    'js': [
-        'static/src/js/jquery-3.6.0.min.js',
-        'static/src/js/velocity.min.js',
-        'static/src/js/velocity.ui.min.js',
-    ],
+    # 'js': [
+    #     'static/src/js/jquery-3.6.0.min.js',
+    #     'static/src/js/velocity.min.js',
+    #     'static/src/js/velocity.ui.min.js',
+    # ],
     'assets': {
         'web.assets_frontend': [
             # Carousel
